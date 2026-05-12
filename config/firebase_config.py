30752f2ef0c32887caf41aa5451be6d966125ed5
KoppalaNaveen <koppalanaveen20@gmail.com>
2026-05-12 16:11:54 +0530

    Success

--- config/firebase_config.py
+++ config/firebase_config.py
@@ -1,7 +1,14 @@
+import os
+import json
 import firebase_admin
+
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_credentials = json.loads(
+    os.environ["FIREBASE_CREDENTIALS"]
+)
+
+cred = credentials.Certificate(firebase_credentials)
 
 firebase_admin.initialize_app(cred)
 
